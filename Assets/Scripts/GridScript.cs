3957ff4a0dd23716892379337cedeb7e365728e7
Alex <a.zhelepov@yandex.ru>
2014-07-21 08:46:20 +0400

    Repository has been created

---
+++ Assets/Scripts/GridScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class GridScript : MonoBehaviour {
+    //=======================ПЕРЕМЕННЫЕ=======================
+    public Vector2 mapInitialPoint; //по умолчанию будет браться (0;0)
+    public Vector2[,] mapGrid; //карта (состоит из 0 и 1)
+    public double gridStep; //шаг сетки 
+    public int width; //ширина сетки
+    public int height; //высота сетки
+    //=======================ПЕРЕМЕННЫЕ=======================
+
+    //=======================ФУНКЦИИ=======================
+    //***
+    //Формирование сетки
+    //***
+    private Vector2[,] FormGrid(int fWidth, int fHeight, double fGridStep, Vector2 fInitialPoint)
+    {
+        Vector2[,] map = new Vector2[fWidth, fHeight];
+
+        for (int i = 0; i < fHeight; i++)
+        {
+            for (int j = 0; j < fWidth; j++)
+            {
+                map[i, j] = new Vector2((float)(i * fGridStep + fGridStep/2), (float)(j * fGridStep + fGridStep/2));
+            }
+        }
+
+        return map;
+    }
+    //=======================ФУНКЦИИ=======================
+
+    //***
+    //Печать карты в файл
+    //***
+    private void PrintMapToFile(Vector2[,] fMap, string fPath)
+    {
+        using (StreamWriter writer = new StreamWriter(fPath))
+        {
+            for (int i = 0; i < fMap.GetLength(0); i++)
+            {
+                writer.Write("\n");
+                for (int j = 0; j < fMap.GetLength(1); j++)
+                {
+                    writer.Write(fMap[i,j].x + " ; " + fMap[i,j].y + "     ");
+                }
+            }
+        }
+    }
+
+    //Пишем генератор карты по препятствиям
+
+	void Start () {
+        mapGrid = FormGrid(width, height, gridStep, mapInitialPoint);
+        PrintMapToFile(mapGrid, "file.txt");
+        Debug.Log("Formed!");
+        
+	}
+
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello world from VS!");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
