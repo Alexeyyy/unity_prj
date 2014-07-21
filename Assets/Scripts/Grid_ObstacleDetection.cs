77c3a13700d976af8a77b8c214758f525fe84e4e
Darmstadium <post>
2014-07-21 22:21:42 +0400

    GridSystem is almost done

---
+++ Assets/Scripts/Grid_ObstacleDetection.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid_ObstacleDetection : MonoBehaviour {
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		Debug.Log ("!!!!!!");
+		if(collider.tag == "Obstacle") 
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
